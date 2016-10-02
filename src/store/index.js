bc82930dcd8755f92e78d42ac3e57b91974cb272
MinskyNg <minsky199525@gmail.com>
2016-10-02 15:34:41 +0800

    remove dist folder out of control

--- src/store/index.js
+++ src/store/index.js
@@ -19,7 +19,7 @@ const loggerMiddleware = createLogger();
 // 创建一个中间件集合
 const middleware = [thunk, loggerMiddleware];
 
-// 利用compose增强store，这个 store 与 applyMiddleware 和 redux-devtools 一起使用
+// 利用compose增强store，这个store与applyMiddleware和redux-devtools一起使用
 const finalCreateStore = compose(
     applyMiddleware(...middleware),
     DevTools.instrument()
